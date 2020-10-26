822be8ea6a48434350ab778d0b52c88f00a74e54
Bram Vereertbrugghen <bram@adimian.com>
2020-10-26 23:03:06 +0100

    Move more funct to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,20 @@
 import pytest
 
+from bnb.config import Config
+from . import TEST_DATA_DIR, TEST_FILE, TEST_BNOTE
+
+
+@pytest.fixture
+def fpath():
+    return TEST_DATA_DIR / TEST_FILE
+
 
 @pytest.fixture(scope="session")
-def file(self):
-    with open(TEST_DATA_DIR / TEST_FILE) as f:
+def file(fpath):
+    with open(fpath) as f:
         return f.read()
+
+
+@pytest.fixture
+def cfg():
+    return Config(bnote_settings_file=TEST_DATA_DIR / TEST_BNOTE)
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -8,22 +8,20 @@ from . import TEST_FILE, TEST_BNOTE, TEST_DATA_DIR
 
 
 class TestExtractor:
-    def test_it_reads_and_extracts_properly(self):
-        cfg = Config(bnote_settings_file=TEST_DATA_DIR / TEST_BNOTE)
-
+    def test_it_reads_and_extracts_properly(self, cfg, fpath):
         extractor = Extractor(config=cfg)
-        result = extractor.run(TEST_DATA_DIR / TEST_FILE)
+        result = extractor.run(fpath)
 
         assert isinstance(result, ExtractedContent)
-        assert result.path == TEST_DATA_DIR / TEST_FILE
+        assert result.path == fpath
         assert "## Content" in result.markdown
         assert 'title: "Test Note"' in result.metadata
 
-    def test_extracting_missing_markdown_raises(self):
+    def test_extracting_missing_markdown_raises(self, fpath):
         cfg = Config(
             markdown_open="beep-beep", bnote_settings_file=TEST_DATA_DIR / TEST_BNOTE
         )
 
         extractor = Extractor(config=cfg)
         with pytest.raises(BoundsNotFound):
-            extractor.run(TEST_DATA_DIR / TEST_FILE)
+            extractor.run(fpath)
