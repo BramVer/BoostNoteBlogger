662d130e5d96dc8fad3b6357d07a6ffe035dcfd1
Bram Vereertbrugghen <bram@adimian.com>
2020-10-28 17:43:17 +0100

    Write to file and create dirs where needed

--- bnb/writer.py
+++ bnb/writer.py
@@ -1,12 +1,34 @@
+import os
+from pathlib import Path
+
 from bnb.config import Config
+from bnb.exceptions import PathConstructionError
 
 
 class Writer:
     def __init__(self, config=None):
-        self.config = config or Config()
+        self.cfg = config or Config()
+
+    def _create_if_not_present(self, path):
+        if not os.path.exists(path.parent):
+            os.makedirs(path.parent)
+
+    def _create_file(self, path, content):
+        self._create_if_not_present(path)
+        with open(path, "w") as f:
+            f.write(content)
+
+    def _construct_new_path(self, conv_content):
+        injection = self.cfg.output_folder
+        fname = conv_content.filename
+        folder = conv_content.folder
+
+        parts = conv_content.path.absolute().parts
+        notes = parts.index(self.cfg.notes_folder)
+
+        return Path(*parts[:notes]).joinpath(injection, folder, fname)
 
     def run(self, converted_content):
-        # get markdown
-        # build path
         # write
-        pass
+        path = self._construct_new_path(converted_content)
+        self._create_file(path, converted_content.content)
--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
+
 import pytest
 
-from bnb.scanner import Scanner
+from bnb.config import Config
 from bnb.writer import Writer
+from bnb.converter import ConvertedContent
+from bnb.exceptions import PathConstructionError
 
 
 class TestWriter:
@@ -9,9 +14,57 @@ class TestWriter:
         writer = Writer()
         assert hasattr(writer, "run")
 
-    def test_it_processes_files(self, cfg, base_dir):
-        scanner = Scanner(cfg)
+    def test_it_properly_builds_new_paths(self, cfg):
+        writer = Writer(cfg)
+        content = ConvertedContent(
+            cfg=cfg,
+            path=Path("/one/two/three/notes/file.cson"),
+            content="",
+            metadata={
+                "title": "Hello there",
+                "folder": "key_for_folder_two",
+            },
+        )
+        expected = Path("/one/two/three/build/Folder Two/hello_there.html")
+        actual = writer._construct_new_path(content)
+
+        assert expected == actual
+
+    def test_it_creates_folders_when_they_dont_exist(self, cfg, tmpdir):
+        writer = Writer(cfg)
+        path = tmpdir.join("one", "two", "three", "four", "notes", "file.cson")
+        content = ConvertedContent(
+            cfg=cfg,
+            path=path,
+            content="haha",
+            metadata={
+                "title": "Hello there",
+                "folder": "key_for_folder_two",
+            },
+        )
+
+        writer.run(content)
+        assert os.path.exists(tmpdir)
+
+    def test_it_can_write_a_file(self, cfg, tmpdir):
         writer = Writer(cfg)
 
-        files = scanner.run(base_dir)
-        writer.run(files)
+        tmpdir.mkdir(cfg.notes_folder)
+        tmpdir.mkdir(cfg.output_folder)
+        fpath = tmpdir.join(cfg.notes_folder, "file.cson")
+        content = ConvertedContent(
+            cfg=cfg,
+            path=fpath,
+            content="<h1>Hello there</h1>",
+            metadata={
+                "title": "Hello there",
+                "folder": "key_for_folder_two",
+            },
+        )
+        writer.run(content)
+
+        # new_fpath = tmpdir / cfg.output_folder / content.folder / content.filename
+        new_fpath = tmpdir / "build" / "Folder Two" / "hello_there.html"
+        assert new_fpath.exists()
+        with open(new_fpath) as f:
+            assert f.read() == content.content
